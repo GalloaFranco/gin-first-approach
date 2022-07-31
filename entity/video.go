4c0b0ab8c3f26373f32941588d6b819622223fe7
galloafranco <galloafranco@gmail.com>
2022-07-31 12:12:17 -0300

    ▶︎ ️Video entity

---
+++ entity/video.go
@@ -0,0 +1,7 @@
+package entity
+
+type Video struct {
+	Title       string `json:"title"`       // Tag fot json serialization
+	Description string `json:"description"` // Tag fot json serialization
+	URL         string `json:"URL"`         // Tag fot json serialization
+}
