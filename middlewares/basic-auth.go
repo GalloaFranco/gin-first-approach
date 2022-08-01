7c082dcdf71b564078c04691aa176d3ceb270ebe
galloafranco <galloafranco@gmail.com>
2022-08-01 12:53:38 -0300

    ⛔️ Authorization basic middleware

---
+++ middlewares/basic-auth.go
@@ -0,0 +1,11 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func Auth() gin.HandlerFunc {
+	// BasicAuth returns a Basic HTTP Authorization middleware. It takes as argument a map[string]string where
+	// the key is the user name and the value is the password.
+	return gin.BasicAuth(gin.Accounts{
+		"admin": "123",
+	})
+}
