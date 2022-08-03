8739153f57bbe7fd165af29671ffbcea6fc0143c
galloafranco <galloafranco@gmail.com>
2022-08-03 02:14:13 -0300

    ▶︎ Login Service

---
+++ services/login.service.go
@@ -0,0 +1,21 @@
+package services
+
+type ILoginService interface {
+	Login(username string, password string) bool
+}
+
+type loginService struct {
+	authorizedUsername string
+	authorizedPassword string
+}
+
+func NewLoginService() ILoginService {
+	return &loginService{ // Here we should call a DB or make a request to extern API, not leave the values hardcoded
+		authorizedPassword: "admin",
+		authorizedUsername: "123",
+	}
+}
+
+func (loginService loginService) Login(username string, password string) bool {
+	return loginService.authorizedPassword == password && loginService.authorizedUsername == username
+}
