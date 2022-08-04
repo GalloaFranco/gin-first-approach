0052b82baf6c95a4d489935b724213e487292379
galloafranco <galloafranco@gmail.com>
2022-08-04 18:02:42 -0300

    ▶︎ Video Repository implemented in video.service.go

--- services/video.service.go
+++ services/video.service.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/GalloaFranco/gin-first-approach/entities"
+import (
+	"github.com/GalloaFranco/gin-first-approach/entities"
+	"github.com/GalloaFranco/gin-first-approach/repositories"
+)
 
 type IVideoService interface {
 	Save(video entities.Video) entities.Video
@@ -9,22 +12,27 @@ type IVideoService interface {
 
 // Struct that would implement IVideoService
 type videoService struct {
-	videos []entities.Video
+	videoRepository repositories.IVideoRepository
+	videos          []entities.Video
 }
 
 // New Constructor function, to return instance of videoService (impl of interface)
 // This function returns a value of interface type IVideoService, which contains a value of concrete type &videoService.
-func New() IVideoService {
-	return &videoService{}
+func New(repository repositories.IVideoRepository) IVideoService {
+	return &videoService{
+		videoRepository: repository,
+	}
 }
 
 // Save function impl
 func (service *videoService) Save(video entities.Video) entities.Video {
-	service.videos = append(service.videos, video)
+	//service.videos = append(service.videos, video)
+	service.videoRepository.Save(video)
 	return video
 }
 
 // FindAll function impl
 func (service *videoService) FindAll() []entities.Video {
-	return service.videos
+	//return service.videos
+	return service.videoRepository.FindAll()
 }
